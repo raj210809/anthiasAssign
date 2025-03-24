57fca61c47b810d2d5a152be86e59cdc6b6779f3
Dhanraj Kumar <dk7631333778@gmail.com>
2025-03-25 03:09:44 +0530

    feat : add individual balance fetching route

---
+++ SupplyAndTrigger/src/controllers/getbalance.ts
@@ -0,0 +1,21 @@
+import {Request , Response} from "express"
+import {gql , request} from "graphql-request"
+
+export async function getBalance (req: Request , res: Response) {
+    const {id} = req.query
+    console.log(typeof(id))
+    try {
+        const query = gql`
+            query {
+                wallet (id : "${id}") {
+                    balance
+                }
+            }
+        `
+        const data = await request("http://localhost:42069/graphql" , query)
+        res.status(200).json(data)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message : "INTERNAL SERVER ERROR"})
+    }
+}
--- SupplyAndTrigger/src/route/route.ts
+++ SupplyAndTrigger/src/route/route.ts
@@ -3,11 +3,13 @@ import express from "express"
 import {getTriggersAndAlerts} from "../controllers/triggersAndAlerts"
 import {getHoldersAndBalances} from "../controllers/holdersAndBalances"
 import {getTotalSupply} from "../controllers/aggregateSupply"
+import {getBalance} from "../controllers/getbalance"
 
 const router = express.Router()
 
 router.get("/triggersAndAlerts" , getTriggersAndAlerts)
 router.get("/holdersAndBalances" , getHoldersAndBalances)
 router.get("/totalSupply" , getTotalSupply)
+router.get("/balance" , getBalance)
 
 export default router
